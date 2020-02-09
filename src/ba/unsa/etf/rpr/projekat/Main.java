d3a299f47e5e3a808b7473d2a79f92749a8e64a2
ehadzic <44297122+ehadzic@users.noreply.github.com>
2020-02-09 15:10:32 +0100

    Initial commit.

--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("E Index");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
