176667bce79c68ea4e2fd4b6f6c3e6db6c5088b4
ehadzic <44297122+ehadzic@users.noreply.github.com>
2020-02-12 14:11:04 +0100

    Zavrsen admin.fxml

--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -14,9 +16,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("E Index");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
     }
 }
